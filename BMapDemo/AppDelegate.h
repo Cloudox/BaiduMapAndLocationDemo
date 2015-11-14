fc2dafa6fbd1c447a76781c38d3785ed042a7790
Cloudox <Cloudox@cloudoxdemacbook-pro.local>
2015-11-14 20:32:54 +0800

    Initial Commit

---
+++ BMapDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BMapDemo
+//
+//  Created by Cloudox on 15/11/14.
+//  Copyright © 2015年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BMapDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BMapDemo
+//
+//  Created by Cloudox on 15/11/14.
+//  Copyright © 2015年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
